8d1943223777509235f6b1655c858cc4b7949177
Alison Clarke <aclarke@scottlogic.co.uk>
2014-09-01 15:53:17 +0100

    Created dummy project

---
+++ rename.sh
@@ -0,0 +1,31 @@
+newName=$1
+if [ -z "$newName" ]; then
+  echo "Please specify a new project name"
+  exit 1
+fi
+
+echo "Renaming MyPlayProject to $newName"
+
+echo "Moving submodules"
+git mv MyPlayProject/ShinobiPlayUtils $newName
+
+echo "Renaming directories"
+git mv MyPlayProject $newName
+git mv "$newName/MyPlayProject" "$newName/$newName"
+
+echo "Renaming files"
+for file in $(git ls-files | grep MyPlayProject | sed -e 's/\(MyPlayProject[^/]*\).*/\1/' | uniq); do
+  git mv $file `echo $file | sed -e "s/MyPlayProject/$newName/"`
+done
+
+echo "Replacing text inside files"
+for file in $(git ls-files | grep -v rename); do
+  if [ -f $file ]; then
+    echo "Replacing text in $file"
+    sed -i '' "s@MyPlayProject@$newName@g" $file
+    git add $file
+  fi
+done
+
+echo "Removing the utility script"
+git rm rename.sh
