2cb0cc09d2700ad3533850aa955ff5a39816e3d8
Daniel Allsop <DAllsop@scottlogic.com>
2015-01-05 08:53:12 +0000

    Added Annotations and Crosshair demos

---
+++ ChartsGallery/ChartsGallery/ChartsGalleryCrosshairDataSource.m
@@ -0,0 +1,55 @@
+//
+//  ChartsGalleryCrosshairDataSource.m
+//  ChartsGallery
+//
+//  Created by Daniel Allsop on 16/12/2014.
+//
+//  Copyright 2014 Scott Logic
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//  http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+//
+
+#import "ChartsGalleryCrosshairDataSource.h"
+
+@implementation ChartsGalleryCrosshairDataSource
+
+- (instancetype)init {
+  self = [super initWithDataFromFile:@"ChartsGallery-running-data"];
+  if (self) {
+    self.seriesNames = @[@"Heart Rate"];
+    self.dateComponents.minute = 1;
+  }
+  return self;
+}
+
+- (id)xValueAtIndex:(NSInteger)dataIndex forSeriesAtIndex:(NSInteger)seriesIndex {
+  self.dateComponents.minute = dataIndex + 0.25;
+  return [self.calendar dateFromComponents:self.dateComponents];
+}
+
+#pragma mark - SChartDatasource methods
+- (SChartSeries *)sChart:(ShinobiChart *)chart seriesAtIndex:(NSInteger)index {
+  SChartLineSeries *series = [SChartLineSeries new];
+  series.title = self.seriesNames[index];
+  series.style.lineColor = [UIColor colorWithRed:230.0f/255 green:41.0f/255 blue:77.0f/255 alpha:1];
+  series.crosshairEnabled = YES;
+  series.style.lineColor = [UIColor colorWithRed:230.0f/255 green:41.0f/255 blue:77.0f/255 alpha:1];
+  series.style.lineWidth = @5;
+  series.style.pointStyle.showPoints = YES;
+  series.style.pointStyle.radius = @20;
+  series.style.pointStyle.innerRadius = @10;
+  series.style.pointStyle.color = [UIColor colorWithRed:230.0f/255 green:41.0f/255 blue:77.0f/255 alpha:1];
+  return series;
+}
+
+@end
---
+++ ChartsGallery/ChartsGallery/ChartsGalleryCustomCrosshairTooltip.m
@@ -0,0 +1,115 @@
+//
+//  ChartsGalleryCustomCrosshairTooltip.m
+//  ChartsGallery
+//
+//  Created by Daniel Allsop on 17/12/2014.
+//
+//  Copyright 2014 Scott Logic
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//  http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+//
+
+#import "ChartsGalleryCustomCrosshairTooltip.h"
+#import <ShinobiCharts/SChartCanvas.h>
+#import "ChartsGalleryCrosshairDataSource.h"
+
+static const CGFloat StockChartTooltipLabelPadding = 5.f;
+
+@interface ChartsGalleryCustomCrosshairTooltip ()
+
+@property (strong, nonatomic) NSDateFormatter *dateFormatter;
+
+@property (strong, nonatomic) UILabel *heartRate;
+@property (strong, nonatomic) UILabel *pace;
+
+@property (strong, nonatomic) NSArray *allLabels;
+
+@end
+
+@implementation ChartsGalleryCustomCrosshairTooltip
+
+- (instancetype)init {
+  self = [super init];
+  if (self) {
+    self.dateFormatter = [NSDateFormatter new];
+    [self.dateFormatter setDateStyle:NSDateFormatterNoStyle];
+    [self.dateFormatter setTimeStyle:NSDateFormatterShortStyle];
+    
+    self.heartRate = [self createTooltipLabel];
+    [self addSubview:self.heartRate];
+    
+    self.pace = [self createTooltipLabel];
+    [self addSubview:self.self.pace];
+    
+    self.allLabels = @[self.label, self.heartRate, self.pace];
+  }
+  return self;
+}
+
+// Method to create a label to add to the tooltip
+- (UILabel*)createTooltipLabel {
+  UILabel *newLabel = [[UILabel alloc] initWithFrame:CGRectZero];
+  newLabel.backgroundColor = [UIColor clearColor];
+  return newLabel;
+}
+
+- (void)setDataPoint:(id<SChartData>)dataPoint fromSeries:(SChartSeries *)series fromChart:(ShinobiChart *)chart {
+  
+  SChartDataPoint *chartdataPoint = ((SChartDataPoint*)dataPoint);
+  
+  // Get a date string from the data point and display as label text
+  self.label.text = [NSString stringWithFormat:@"%@", [self.dateFormatter stringFromDate:chartdataPoint.xValue]];
+  
+  NSInteger dataPointIndex = chartdataPoint.index;
+  ChartsGalleryCrosshairDataSource *dataSource = chart.datasource;
+  NSDictionary *dataCollection = dataSource.dataCollection[dataPointIndex];
+  self.heartRate.text = [NSString stringWithFormat:@"Heart Rate: %@", dataCollection[@"Heart Rate"]];
+  self.pace.text = [NSString stringWithFormat:@"Pace: %@", dataCollection[@"Pace"]];
+}
+
+- (void)setPosition:(struct SChartPoint)position onCanvas:(SChartCanvas*)canvas {
+  [super setPosition:position onCanvas:canvas];
+  
+  // Lay out the labels, keeping track of the maximum width
+  CGFloat maxLabelWidth = 0;
+  CGFloat labelYPosition = StockChartTooltipLabelPadding;
+  
+  for (UILabel *label in self.allLabels) {
+    // Position the label
+    label.font = [UIFont fontWithName:@"HelveticaNeue-Thin" size:12];
+    [label sizeToFit];
+    CGRect frame = label.frame;
+    frame.origin = CGPointMake(StockChartTooltipLabelPadding, labelYPosition);
+    label.frame = frame;
+    
+    maxLabelWidth = MAX(maxLabelWidth, label.frame.size.width);
+    labelYPosition += label.frame.size.height;
+  }
+  
+  // Further customize top most label in crosshair tooltip
+  UILabel *topMostLabel = self.allLabels[0];
+  topMostLabel.font = [UIFont fontWithName:@"HelveticaNeue-Thin" size:14];
+  CGRect labelFrame = topMostLabel.frame;
+  labelFrame.size.width =  maxLabelWidth;
+  topMostLabel.frame = labelFrame;
+  
+  // Resize tooltip frame
+  CGRect frame = self.frame;
+  frame.size.width = maxLabelWidth + (2 * StockChartTooltipLabelPadding);
+  frame.size.height = labelYPosition + StockChartTooltipLabelPadding;
+  frame.origin.y = canvas.glView.frame.origin.y;
+  frame.origin.x = position.x - (frame.size.width / 2);
+  self.frame = frame;
+}
+
+@end
